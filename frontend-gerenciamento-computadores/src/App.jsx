7bbc5cd84dd0a7cbd9c950b9153e8ffecff3720b
Ubuntu <azureuser@supp-developer-3.sitbjkltn5ouvipbbztmib1whe.nx.internal.cloudapp.net>
2025-07-17 00:55:54 +0000

    fix app.jsx

--- frontend-gerenciamento-computadores/src/App.jsx
+++ frontend-gerenciamento-computadores/src/App.jsx
@@ -15,7 +15,7 @@ import Login from '@/components/Login.jsx'
 import './App.css'
 
       
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 // Função para formatar o número de patrimônio
 const formatPatrimonio = (patrimonio) => {
